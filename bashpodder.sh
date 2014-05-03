1055323037059eaf7f42e1316a8dd6e22904f57e
Sergei Dolgov <dolgovs@gmail.com>
2014-05-03 17:18:08 +0200

    Use python3

--- bashpodder.sh
+++ bashpodder.sh
@@ -51,7 +51,7 @@ download_file () {
 }
 
 unquote () {
-    python -c "import urllib, sys; print urllib.unquote(sys.argv[1])" $1
+    python3 -c "import urllib.parse, sys; print(urllib.parse.unquote(sys.argv[1]))" $1
 }
 
 main () {
